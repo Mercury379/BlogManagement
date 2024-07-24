7fe3f0401b736edd89ebfe9efe54ecd58979ffd3
rzt <2010816612@qq.com>
2024-07-24 11:14:38 +0800

    feat: 新增广告图片上传功能

--- newland-blog/newland-blog-article/src/main/java/com/newland/blog/article/controller/AdvertController.java
+++ newland-blog/newland-blog-article/src/main/java/com/newland/blog/article/controller/AdvertController.java
@@ -4,12 +4,18 @@ package com.newland.blog.article.controller;
 import com.newland.blog.article.req.AdvertREQ;
 import com.newland.blog.article.service.IAdvertService;
 import com.newland.blog.entities.Advert;
+import com.newland.blog.util.aliyun.AliyunUtil;
 import com.newland.blog.util.base.Result;
+import com.newland.blog.util.enums.PlatformEnum;
+import com.newland.blog.util.properties.AliyunProperties;
+import com.newland.blog.util.properties.BlogProperties;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
 
@@ -21,11 +27,14 @@ import java.util.Date;
 @Api(value = "广告管理接口", description = "广告管理接口,提供广告的增、删、改、查")
 @RestController
 @RequestMapping("/advert")
+@Validated
 public class AdvertController {
 
     @Autowired
     private IAdvertService advertService;
 
+    @Autowired
+    private BlogProperties blogProperties;
 
     @ApiOperation("根据广告标题与状态查询广告分页列表接口")
     @PostMapping("/search") // /article/advert/search
@@ -57,8 +66,19 @@ public class AdvertController {
 
     @ApiOperation("新增广告信息接口")
     @PostMapping
-    public Result save(@RequestBody Advert advert) {
+
+    public Result save(@Validated Advert advert, @RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            // 将文件保存到OSS服务器
+            AliyunProperties aliyun = blogProperties.getAliyun();
+            Result fileRes = AliyunUtil.uploadFileToOss(PlatformEnum.ARTICLE, file, aliyun);
+            if(fileRes.getCode() != 20000) {
+                return Result.error(fileRes.getMessage());
+            }
+            advert.setImageUrl(fileRes.getData().toString());
+        }
         advertService.save(advert);
+
         return Result.ok();
     }
 
